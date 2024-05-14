12ca66ac9a2a8f73d2be2be8a487eb11d7a2c649
Zeitcatcher <ars.oksenoyt@gmail.com>
2024-05-13 17:12:56 -1000

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Arseniy Oksenoyt on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
